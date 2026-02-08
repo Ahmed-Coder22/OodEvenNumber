239a4d3b5c00ffd5b2dae46df65d678f2ead2506
Ahmed Sultan <mo5359899@gmail.com>
2026-02-08 20:51:49 +0200

    initial commit

---
+++ OodEvenNumberApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>() { 10,11,12,20,31,5,6};
+            Console.Write($"Even Numbers : {string.Join(' ',Numbers.Where(n =>IsEven(n)))}");
+            Console.WriteLine();
+            Console.Write($"OOd Numbers : {string.Join(' ',Numbers.Where(n =>IsOod(n)))}");
+            Console.WriteLine();
+        }
+
+        static bool IsEven(int value)
+        {
+            if (value % 2 == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        static bool IsOod(int value)
+        {
+            if (value % 2 != 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+    }
+}
